4aa44d892a1e905a596a2fee48f340a489ab7b2a
Jyoti Savani <savanijyoti4@gmail.com>
2021-02-27 17:23:23 +0530

    Created file upload API for global.
    It will be used in all platform(ionic, angular, capacitor, cordova, etc)

---
+++ upload-file.php
@@ -0,0 +1,59 @@
+<?php 
+header('Content-Type: application/json; charset=utf-8');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: PUT, GET, POST");
+
+
+
+$response = array();
+$upload_dir = 'uploads/';
+$server_url = 'http://127.0.0.1:8888';
+if($_FILES['file'])
+{
+    $file_name = $_FILES["file"]["name"];
+    $file_tmp_name = $_FILES["file"]["tmp_name"];
+    $error = $_FILES["file"]["error"];
+
+    if($error > 0){
+        $response = array(
+            "status" => "error",
+            "error" => true,
+            "message" => "Error uploading the file!"
+        );
+    }else 
+    {
+        $random_name = rand(1000,1000000)."-".$file_name;
+        $upload_name = $upload_dir.strtolower($random_name);
+        $upload_name = preg_replace('/\s+/', '-', $upload_name);
+    
+        if(move_uploaded_file($file_tmp_name , $upload_name)) {
+            $response = array(
+                "status" => "success",
+                "error" => false,
+                "message" => "File uploaded successfully",
+                "url" => $server_url."/".$upload_name
+              );
+        }else
+        {
+            $response = array(
+                "status" => "error",
+                "error" => true,
+                "message" => "Error uploading the file!"
+            );
+        }
+    }
+
+
+
+    
+
+}else{
+    $response = array(
+        "status" => "error",
+        "error" => true,
+        "message" => "No file was sent!"
+    );
+}
+
+echo json_encode($response);
+?>
